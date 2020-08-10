f02ee3465cf1e2a9908282ddcdc0de85d89ef66e
Hideki Kawahara <kawahara@sys.wakayama-u.ac.jp>
2020-08-10 20:31:41 +0900

    Removed confusing versions
    Removed confusing versions

--- ieiceAndAsj2020March/foToScorePosition.m
+++
@@ -1,9 +0,0 @@
-function scoreViewPosition = foToScorePosition(fo)
-% convert fo to position on the musical score
-scoreLocation = 0:32;
-chromaticNoteId = [0 2 3 5 7 8 10  [0 2 3 5 7 8 10] + 12  ...
-    [0 2 3 5 7 8 10] + 24 [0 2 3 5 7 8 10] + 36 [0 2 3 5 7] + 48];
-scoreViewPosition = interp1(chromaticNoteId, scoreLocation, ...
-    log2(fo / 55) * 12, "linear", "extrap");
-end
-
